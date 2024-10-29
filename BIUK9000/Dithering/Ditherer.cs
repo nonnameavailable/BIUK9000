f47b08453840029021afa667422b60765de4bdca
nnnHome <nonnameavailable@gmail.com>
2024-10-29 20:13:34 +0100

    Started work on dithering

---
+++ BIUK9000/Dithering/Ditherer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AnimatedGif.Quantizer;
+
+namespace BIUK9000.Dithering
+{
+    public class Ditherer
+    {
+        public FastBitmap Fbm {  get; set; }
+        public Ditherer(Bitmap bitmap) 
+        {
+            Fbm = new FastBitmap(bitmap);
+        }
+
+        private double ColorDistance(int c1, int c2)
+        {
+            // Extract RGB components from the first color
+            byte r1 = (byte)((c1 >> 16) & 0xFF);
+            byte g1 = (byte)((c1 >> 8) & 0xFF);
+            byte b1 = (byte)(c1 & 0xFF);
+
+            // Extract RGB components from the second color
+            byte r2 = (byte)((c2 >> 16) & 0xFF);
+            byte g2 = (byte)((c2 >> 8) & 0xFF);
+            byte b2 = (byte)(c2 & 0xFF);
+
+            // Calculate the Euclidean distance
+            double distance = Math.Sqrt(
+            Math.Pow(r1 - r2, 2) +
+            Math.Pow(g1 - g2, 2) +
+            Math.Pow(b1 - b2, 2)
+            );
+
+            return distance;
+        }
+    }
+}
---
+++ BIUK9000/Dithering/FastBitmap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIUK9000.Dithering
+{
+    //from https://stackoverflow.com/a/34801225/9852011
+    public class FastBitmap
+    {
+        public Bitmap Bitmap { get; private set; }
+        public Int32[] Bits { get; private set; }
+        public bool Disposed { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        protected GCHandle BitsHandle { get; private set; }
+
+        public FastBitmap(Bitmap bitmap)
+        {
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+            Bits = new Int32[Width * Height];
+            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+            Bitmap = new Bitmap(Width, Height, Width * 4, PixelFormat.Format32bppArgb, BitsHandle.AddrOfPinnedObject());
+            using Graphics g = Graphics.FromImage(Bitmap);
+            g.DrawImage(bitmap, 0, 0);
+        }
+
+        public void SetPixel(int x, int y, Color colour)
+        {
+            int index = x + (y * Width);
+            int col = colour.ToArgb();
+
+            Bits[index] = col;
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int index = x + (y * Width);
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+        public int GetPixelAsInt(int x, int y)
+        {
+            return Bits[x + (y * Width)];
+        }
+
+        public void Dispose()
+        {
+            if (Disposed) return;
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
+        }
+    }
+}
